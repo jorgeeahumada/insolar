7774b5753367bbe12a753c1a91d748bab4678bdc
Jorge Eduardo Ahumada <e.ahumada.hdz@gmail.com>
2018-10-11 14:06:26 -0700

    Work thorught half of the installers page, the design is still pretty basic but I'm hoping to finish the page looking simple and then go back and improve the design and interactions.

--- installers.php
+++ installers.php
@@ -6,7 +6,7 @@
     ================================================== -->
     <meta charset="utf-8">
     <title>Insolar | Join our network and expand your business</title>
-    <meta name="description" content=" Learn the in’s and out’s of our network of solar installers. Don’t waste time on dead leads, get connected with hundreds of customers, ready to install solar energy. Learn how you can expand your business, and decrease your soft cost with Insolar. Get access to hundreds of data points about your market, and learn how to always stay competitive.">
+    <meta name="description" content="Learn the in’s and out’s of our network of solar installers. Don’t waste time on dead leads, get connected with hundreds of customers, ready to install solar energy. Learn how you can expand your business, and decrease your soft cost with Insolar. Get access to hundreds of data points about your market, and learn how to always stay competitive.">
     <meta name="author" content="Jorge Eduardo Ahumada Hernandez">
 
     <!-- mobile specific metas
@@ -27,6 +27,7 @@
     <link rel="stylesheet" href="css/navigation_bar.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/preloader.css">
+    <link rel="stylesheet" href="css/installers.css">
 
     <!-- script
     ================================================== -->
@@ -51,68 +52,215 @@
         include 'navigation_bar.php';
     ?>
 
-    <!-- s-content
+    <!-- call-to-action
+    ================================================== -->
+    <section class="s-banner">
+        <div class="banner jumbotron">
+            <div class="wrapper">
+                <h1 class="display-1 display-1--light">Join our network</h1>
+            </div>
+            <div class="wrapper">
+                <a class="btn btn--primary" href="#0">Sign Up</a>
+            </div>
+        </div>
+    </section> <!-- s-banner -->
+
+    <!-- why insolar
+    ================================================== -->
+    <section class="s-content s-content--white">
+        <div class="timeline">
+            <div class="timeline-container timeline-container__left">
+                <div class="content">
+                    <h3 class="subhead">No more dead leads, get right to business.</h3>
+                    <p>
+                        By joining our Insolar platform you get immediate
+                        access to our hundreds of daily customers. We eliminate
+                        the need to follow hundreds of dead leads, by delivering
+                        only customers, ready to buy.
+                    </p>
+                </div>
+            </div>
+            <div class="timeline-container timeline-container__right">
+                <div class="content">
+                    <h3 class="subhead">
+                        With our AI system we match our customers,
+                        with the best installer in their area, led that be you.
+                    </h3>
+                    <p>
+                        As part of our network you get access to hundreds of
+                        data points about your market. Our tools allow you to
+                        stay competitive, and grow your business. By cutting
+                        most of the soft cost related to operations, you will
+                        be able to focus on delivering a one of a kind customer
+                        experience, increasing your Insolar ranking and your customer flow.
+                    </p>
+                </div>
+            </div>
+            <div class="timeline-container timeline-container__left">
+                <div class="content">
+                    <h3 class="subhead">Enjoy our trouble free partner experience.</h3>
+                    <p>
+                        At the core, we are a software company with a focus on user
+                        experience, and that translates to our partners. Enjoy our easy
+                        to use web interface wherever you are. Easily accept or decline
+                        offers. Manage all of your current customers, from appointments
+                        to the installation process, all in one page. Explore your local
+                        market and discover new ways to stay on top of the competition.
+                        All from our expertly design interface.
+                    </p>
+                </div>
+            </div>
+        </div> <!-- timeline -->
+    </section> <!-- why insolar -->
+
+    <!-- 3 easy steps
+    ================================================== -->
+    <section class="s-content s-content--narrow s-content--white">
+        <div class="row">
+            <div class="wrapper">
+                <h3 class="display-2 display-2--pink">Insolar is as simple as 1, 2, 3</h3>
+            </div>
+            <div class="container">
+                <div class="row s-steps">
+                    <div class="col-md-4 step-content">
+                        <div class="wrapper">
+                            <h4 class="display-1">1</h4>
+                        </div>
+                        <div class="wrapper">
+                            <p>A customer accepts our solar quote and makes an initial deposit.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 step-content step-content--middle">
+                        <div class="wrapper">
+                            <h4 class="display-1">2</h4>
+                        </div>
+                        <div class="wrapper">
+                            <p>Our top installers gets a notification to either accept or denied our quote.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 step-content">
+                        <div class="wrapper">
+                            <h4 class="display-1">3</h4>
+                        </div>
+                        <div class="wrapper">
+                            <p>The installer meets with the customer and starts working.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> <!-- 3 easy steps -->
+
+    <!-- sign up
     ================================================== -->
     <section class="s-content s-content--narrow">
 
-        <div class="row">
+        <div class="overlay"></div>
 
-            <div class="s-content__header col-full">
-                <h1 class="s-content__header-title">
-                    Learn More About Us.
-                </h1>
-            </div> <!-- end s-content__header -->
+        <div class="row section-header" data-aos="fade-up">
+            <div class="col-full">
+                <h3 class="subhead">Ready to join us?</h3>
+            </div>
+        </div>
 
-            <div class="s-content__media col-full">
-                <div class="s-content__post-thumb">
-                    <img src="images/thumbs/about/about-1000.jpg" 
-                         srcset="images/thumbs/about/about-2000.jpg 2000w, 
-                                 images/thumbs/about/about-1000.jpg 1000w, 
-                                 images/thumbs/about/about-500.jpg 500w" 
-                         sizes="(max-width: 2000px) 100vw, 2000px" alt="" >
-                </div>
-            </div> <!-- end s-content__media -->
+        <div class="row contact-content" data-aos="fade-up">
 
-            <div class="col-full s-content__main">
+            <div class="contact-primary">
+                <form name="contactForm" id="contactForm" method="post" action="" novalidate="novalidate">
+                    <fieldset>
 
-                <p class="lead">Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint.</p>
-                
-                <p>Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum adipisicing aliqua ea nisi sint ut quis proident ullamco ut dolore culpa occaecat ut laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat occaecat fugiat in adipisicing in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed.
-                </p>
+                        <div class="form-field">
+                            <input name="contactName" type="text" id="contactName" placeholder="Your Name" value="" minlength="2" required="" aria-required="true" class="full-width">
+                        </div>
+                        <div class="form-field">
+                            <input name="contactEmail" type="email" id="contactEmail" placeholder="Your Email" value="" required="" aria-required="true" class="full-width">
+                        </div>
+                        <div class="form-field">
+                            <input name="contactSubject" type="text" id="contactSubject" placeholder="Subject" value="" class="full-width">
+                        </div>
+                        <div class="form-field">
+                            <textarea name="contactMessage" id="contactMessage" placeholder="Your Message" rows="10" cols="50" required="" aria-required="true" class="full-width"></textarea>
+                        </div>
+                        <div class="form-field">
+                            <button class="full-width btn--primary">Sign Up</button>
+                            <div class="submit-loader">
+                                <div class="text-loader">Sending...</div>
+                                <div class="s-loader">
+                                    <div class="bounce1"></div>
+                                    <div class="bounce2"></div>
+                                    <div class="bounce3"></div>
+                                </div>
+                            </div>
+                        </div>
 
-                <p>Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco.
-                </p>
+                    </fieldset>
+                </form>
 
-                <div class="row block-1-2 block-tab-full">
-                    <div class="col-block">
-                        <h3 class="quarter-top-margin">Who We Are.</h3>
-                        <p>Lorem ipsum Nisi amet fugiat eiusmod et aliqua ad qui ut nisi Ut aute anim mollit fugiat qui sit ex occaecat et eu mollit nisi pariatur fugiat deserunt dolor veniam reprehenderit aliquip magna nisi consequat aliqua veniam in aute ullamco Duis laborum ad non pariatur sit.</p>
-                    </div>
-
-                    <div class="col-block">
-                        <h3 class="quarter-top-margin">Our Mission.</h3>
-                        <p>Lorem ipsum Nisi amet fugiat eiusmod et aliqua ad qui ut nisi Ut aute anim mollit fugiat qui sit ex occaecat et eu mollit nisi pariatur fugiat deserunt dolor veniam reprehenderit aliquip magna nisi consequat aliqua veniam in aute ullamco Duis laborum ad non pariatur sit.</p>
-                    </div>
-
-                    <div class="col-block">
-                        <h3 class="quarter-top-margin">Our Vision.</h3>
-                        <p>Lorem ipsum Nisi amet fugiat eiusmod et aliqua ad qui ut nisi Ut aute anim mollit fugiat qui sit ex occaecat et eu mollit nisi pariatur fugiat deserunt dolor veniam reprehenderit aliquip magna nisi consequat aliqua veniam in aute ullamco Duis laborum ad non pariatur sit.</p>
-                    </div>
-
-                    <div class="col-block">
-                        <h3 class="quarter-top-margin">Our Values.</h3>
-                        <p>Lorem ipsum Nisi amet fugiat eiusmod et aliqua ad qui ut nisi Ut aute anim mollit fugiat qui sit ex occaecat et eu mollit nisi pariatur fugiat deserunt dolor veniam reprehenderit aliquip magna nisi consequat aliqua veniam in aute ullamco Duis laborum ad non pariatur sit.</p>
-                    </div>
-
+                <!-- contact-warning -->
+                <div class="message-warning">
+                    Something went wrong. Please try again.
                 </div>
 
+                <!-- contact-success -->
+                <div class="message-success">
+                    Your message was sent, thank you!<br>
+                </div>
 
-            </div> <!-- end s-content__main -->
+            </div> <!-- end contact-primary -->
 
-        </div> <!-- end row -->
+            <div class="contact-secondary">
+                <div class="contact-info">
 
-    </section> <!-- s-content -->
+                    <h3 class="h6 hide-on-fullwidth">Sign Up and enjoy our benefits</h3>
 
+                    <div class="cinfo">
+                        <h5>Wide netwrok of clients</h5>
+                    </div>
+
+                    <div class="cinfo">
+                        <h5>No Fees</h5>
+                    </div>
+
+                    <div class="cinfo">
+                        <h5>Market Insights</h5>
+                    </div>
+                    <div class="cinfo">
+                        <h5>Secure Leads</h5>
+                    </div>
+
+                    <ul class="contact-social">
+                        <li>
+                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+                        </li>
+                    </ul> <!-- end contact-social -->
+
+                </div> <!-- end contact-info -->
+            </div> <!-- end contact-secondary -->
+
+        </div> <!-- end contact-content -->
+    </section>
+
+    <!-- questions
+    ================================================== -->
+    <section class="s-content s-content--narrow">
+        <div class="container">
+            <h2>Any Questions?</h2>
+            <h5>Please contact us and we'll help you decide whether Insolar is right for you.</h5>
+            <a href="#0">Contact Us</a>
+        </div>
+    </section>
     <!-- s-footer
     ================================================== -->
     <?php
