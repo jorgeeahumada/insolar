baac2ed3933aca31726f58d2e9b7e3f200c6aabb
Jorge Eduardo Ahumada <e.ahumada.hdz@gmail.com>
2018-10-25 10:26:41 -0700

    Still working on the responsiveness of the proposal page, but I finally solved the problem with the brackets and the dotted lines, so I'm hoping to finish it today.

--- proposal.php
+++ proposal.php
@@ -228,60 +228,106 @@
         <div class="s-finance__price-summary"> <!-- Pricing Summary -->
             <h2 class="h3">Pricing Summary</h2>
             <ul class="list-unstyled">
-                <li class="line__dotted">
-                    <span class="leader blue">System Install</span>
-                    <span class="blue">$ 4,816</span>
+                <li class="row">
+                    <div class="col col-md-3 col-lg-5 col-xl-4">
+                        <h5>System Install</h5>
+                    </div>
+                    <div class="col col-md-6 col-lg-3 col-xl-4 line__dotted"></div>
+                    <div class="col col-md-3 col-lg-4 col-xl-4">
+                        <h4>$ 4,816</h4>
+                    </div>
                 </li>
-                <li class="line__dotted">
-                    <span class="leader blue">System Cost</span>
-                    <span class="blue">$ 3,996</span>
+                <li class="row">
+                    <div class="col col-md-3 col-lg-5 col-xl-4">
+                        <h5>System Cost</h5>
+                    </div>
+                    <div class="col col-md-6 col-lg-3 col-xl-4 line__dotted"></div>
+                    <div class="col col-md-3 col-lg-4 col-xl-4">
+                        <h4>$ 3,996</h4>
+                    </div>
                 </li>
-                <li class="line__dotted">
-                    <span class="leader blue">Gross System Cost</span>
-                    <span class="blue">$ 8,781.90</span>
+                <li class="row">
+                    <div class="col col-md-4 col-lg-6 col-xl-5">
+                        <h5>Gross System Cost</h5>
+                    </div>
+                    <div class="col col-md-5 col-lg-2 col-xl-2 line__dotted"></div>
+                    <div class="col col-md-3 col-lg-4 col-xl-5">
+                        <h4>$ 8,781.90</h4>
+                    </div>
                 </li>
-                <li class="container">
-                    <p class="first__p">Credit Details</p>
-                    <p class="second__p">US Federal Tax Credit (30%)</p>
-                    <div class="line__dotted">
-                        <span class="blue medium">$ 2,643.57</span>
+                <li>
+                    <h5 class="first__p">Credit Details</h5>
+                    <h6 class="second__p">US Federal Tax Credit (30%)</h6>
+                    <div class="row">
+                        <div class="col offset-md-2 col-md-7 col-xl-8 line__dotted"></div>
+                        <div class="col col-md-3 col-xl-4">
+                            <h4>$ 2,643.57</h4>
+                        </div>
                     </div>
                 </li>
             </ul>
             <h3>Net Total</h3>
-            <div class="line__dotted">
-                <span class="blue stand__out">$ 6,147,33</span>
+            <div class="row">
+                <div class="col offset-md-2 col-md-7 col-xl-7 line__dotted"></div>
+                <div class="col col-md-3 col-xl-5">
+                    <h4 class="h2">$ 6,147.33</h4>
+                </div>
             </div>
         </div> <!-- Pricing Summary -->
 
         <div class="s-finance__financial-overview"> <!-- Financing Overview -->
             <h2>Financial Overview</h2>
             <div class="row">
-                <div class="col-md-8 info_container">
+                <div class=" col-md-9 col-lg-8 info_container">
                     <ul class="list-unstyled">
-                        <li class="line__dotted line__dotted-dark">
-                            <span class="leader simple_header cream">Down Payment</span>
-                            <span class="cream bold_header">$ 0</span>
+                        <li class="row">
+                            <div class="col col-md-4 col-lg-6 col-xl-3">
+                                <h4 class="simple_header">Down Payment</h4>
+                            </div>
+                            <div class="col col-md-5 col-lg-2 col-xl-6 line__dotted line__dotted-dark"></div>
+                            <div class="col col-md-3 col-lg-4 col-xl-3">
+                                <h4>$ 0</h4>
+                            </div>
                         </li>
-                        <li class="line__dotted line__dotted-dark">
-                            <span class="leader simple_header cream">Interest Rate</span>
-                            <span class="cream bold_header">4.99%</span>
+                        <li class="row">
+                            <div class="col col-lg-5 col-xl-3">
+                                <h4 class="simple_header">Interest Rate</h4>
+                            </div>
+                            <div class="col col-lg-3 col-xl-6 line__dotted line__dotted-dark"></div>
+                            <div class="col col-lg-4 col-xl-3">
+                                <h4>4.99%</h4>
+                            </div>
                         </li>
-                        <li class="line__dotted line__dotted-dark">
-                            <span class="leader simple_header cream">Loan Period</span>
-                            <span class="cream bold_header">20 Years</span>
+                        <li class="row">
+                            <div class="col col-lg-5 col-xl-3">
+                                <h4 class="simple_header">Loan Period</h4>
+                            </div>
+                            <div class="col col-lg-3 col-xl-6 line__dotted line__dotted-dark"></div>
+                            <div class="col col-lg-4 col-xl-3">
+                                <h4>20 Years</h4>
+                            </div>
                         </li>
-                        <li class="line__dotted line__dotted-dark">
-                            <span class="leader simple_header cream">Loan Amount</span>
-                            <span class="cream bold_header">$ 9,979</span>
+                        <li class="row">
+                            <div class="col col-lg-5 col-xl-3">
+                                <h4 class="simple_header">Loan Amount</h4>
+                            </div>
+                            <div class="col col-lg-3 col-xl-6 line__dotted line__dotted-dark"></div>
+                            <div class="col col-lg-4 col-xl-3">
+                                <h4>$ 9,979</h4>
+                            </div>
                         </li>
-                        <li class="line__dotted line__dotted-dark">
-                            <span class="leader simple_header cream">Credits/Rebates</span>
-                            <span class="cream bold_header">$ 2,994</span>
+                        <li class="row">
+                            <div class="col col-lg-6 col-xl-3">
+                                <h4 class="simple_header">Credits/Rebates</h4>
+                            </div>
+                            <div class="col col-lg-2 col-xl-6 line__dotted line__dotted-dark"></div>
+                            <div class="col col-lg-4 col-xl-3">
+                                <h4>$ 2,994</h4>
+                            </div>
                         </li>
                     </ul>
                 </div>
-                <div class="col-md-4 banners">
+                <div class=" col-md-3 col-lg-4 banners">
                     <ul class="stand__outs list-unstyled">
                         <li>
                             <h3 class="display-2 display-2--pink">$ 46.06</h3>
